1a0ea4bbe67b0d116d4d97b622263c387df54236
Daniel Cunha <danielbrandaodacunha@gmail.com>
2017-05-05 14:05:19 -0300

    Begin

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api/v1/', todos);
+
+
+app.listen(3000, function(){
+    console.log('Server startes on port 3000...');
+})
+
